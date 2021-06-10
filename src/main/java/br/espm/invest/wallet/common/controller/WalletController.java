6d68c98274f02796848a719e330ae4df2a2aa44b
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2021-06-10 10:55:33 -0300

    add transaction bean

--- src/main/java/br/espm/invest/wallet/common/controller/WalletController.java
+++ src/main/java/br/espm/invest/wallet/common/controller/WalletController.java
@@ -1,11 +1,31 @@
 package br.espm.invest.wallet.common.controller;
 
+import br.espm.invest.wallet.common.datatype.ExchangeTransaction;
+import br.espm.invest.wallet.common.datatype.TransactionBean;
 import br.espm.invest.wallet.common.datatype.Wallet;
+import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient("wallet-service")
 public interface WalletController {
+
+    @GetMapping("wallet")
+    List<Wallet> wallets();
+
     @GetMapping("wallet/{id}")
     Wallet wallet(String id);
+
+    @PostMapping("wallet")
+    Wallet create(@RequestBody Wallet wallet);
+
+    @PostMapping("wallet/{id}/exchange/buy")
+    ExchangeTransaction exchangeBuy(@RequestBody TransactionBean bean);
+
+    @PostMapping("wallet/{id}/exchange/sell")
+    ExchangeTransaction exchangeSell(@RequestBody TransactionBean bean);
 }
---
+++ src/main/java/br/espm/invest/wallet/common/datatype/TransactionBean.java
@@ -0,0 +1,7 @@
+package br.espm.invest.wallet.common.datatype;
+
+public class TransactionBean {
+    private String symbol;
+    private double amount;
+    private double limit;
+}
